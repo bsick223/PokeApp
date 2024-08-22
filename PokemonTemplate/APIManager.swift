6928c07aa3bcda1ee0a926e4ab16e2e94c6aa6e2
Brendan Sick <brendansick@gmail.com>
2024-08-21 22:14:44 -0700

    Initialized commit

---
+++ PokemonTemplate/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  PokemonTemplate
+//
+//  Created by Mark Kinoshita on 8/13/24.
+//
+
+import Foundation
---
+++ PokemonTemplate/PokemonDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDetailView.swift
+//  PokemonTemplate
+//
+//  Created by Brendan Sick on 8/21/24.
+//
+
+import Foundation
